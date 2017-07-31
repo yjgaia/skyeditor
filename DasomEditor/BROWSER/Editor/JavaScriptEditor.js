7484ceacab167c2c76884e75549ce5a25561d981
YJ <hanul@hanul.me>
2017-08-01 02:34:05 +0900

    no message

---
+++ DasomEditor/BROWSER/Editor/JavaScriptEditor.js
@@ -0,0 +1,11 @@
+DasomEditor.JavaScriptEditor = CLASS({
+
+	preset : () => {
+		return DasomEditor.Editor;
+	},
+
+	init : (inner, self) => {
+		
+		
+	}
+});
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DasomEditor
 다솜 에디터
+
+- [Electron](https://electron.atom.io/) 사용
+- 파일 드래그 앤 드롭으로 복사되는 기능 제공, Ctrl C, Ctrl V로 복사되도록
+- 탭 기능
+- FTP 기능
