ad2e123676919090c62d6ce04c91420496892fe0
Hanul <hanul@hanul.me>
2017-11-19 16:22:00 +0900

    let var 추가

--- DasomEditor/R/js/ace/mode-nsp.js
+++ DasomEditor/R/js/ace/mode-nsp.js
@@ -1007,16 +1007,15 @@ var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
 var JavaHighlightRules = function() {
     var keywords = (
-    "abstract|continue|for|new|switch|" +
-    "assert|default|goto|package|synchronized|" +
-    "boolean|do|if|private|this|" +
-    "break|double|implements|protected|throw|" +
-    "byte|else|import|public|throws|" +
-    "case|enum|instanceof|return|transient|" +
-    "catch|extends|int|short|try|" +
-    "char|final|interface|static|void|" +
-    "class|finally|long|strictfp|volatile|" +
-    "const|float|native|super|while"
+    "continue|for|new|switch|" +
+    "do|if|this|" +
+    "break|throw|" +
+    "else|import|throws|" +
+    "case|return|" +
+    "catch|extends|let|var|try|" +
+    "class|" +
+    "const|while|" +
+    "print|include|pause|resume|cookie|self"
     );
 
     var buildinConstants = ("null|Infinity|NaN|undefined");
