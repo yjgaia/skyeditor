d5e8521d627d66dadb6b3de8fdecb1b7e811543d
YJ <hanul@hanul.me>
2018-05-12 01:37:03 +0900

    Git 연동 작업중

--- DasomEditor/BROWSER/Editor/GitEditor.js
+++ DasomEditor/BROWSER/Editor/GitEditor.js
@@ -20,12 +20,69 @@ DasomEditor.GitEditor = CLASS((cls) => {
 				src : DasomEditor.R('icon/git.png')
 			}));
 			
-			DasomEditor.IDE.gitDiff(folderPath, {
-				error : (errorMsg) => {
-					console.log(errorMsg);
-				},
-				success : (newFilePaths, updatedFilePaths, removedFilePaths) => {
-					console.log(newFilePaths, updatedFilePaths, removedFilePaths);
+			// 깃 저장소인지 체크
+			DasomEditor.IDE.checkExists({
+				path : folderPath + '/.git'
+			}, (exists) => {
+				
+				if (exists === true) {
+					
+					DasomEditor.IDE.gitDiff(folderPath, {
+						error : (errorMsg) => {
+							console.log(errorMsg);
+						},
+						success : (newFilePaths, updatedFilePaths, movedFilePaths, removedFilePaths) => {
+							
+							EACH(updatedFilePaths, (path) => {
+								self.append(UUI.BUTTON_H({
+									icon : IMG({
+										src : DasomEditor.R('icon/edit.png')
+									}),
+									spacing : 5,
+									title : path
+								}));
+							});
+							
+							EACH(newFilePaths, (path) => {
+								self.append(UUI.BUTTON_H({
+									icon : IMG({
+										src : DasomEditor.R('icon/add.png')
+									}),
+									spacing : 5,
+									title : path
+								}));
+							});
+							
+							EACH(movedFilePaths, (path) => {
+								self.append(UUI.BUTTON_H({
+									icon : IMG({
+										src : DasomEditor.R('icon/move.png')
+									}),
+									spacing : 5,
+									title : path
+								}));
+							});
+							
+							EACH(removedFilePaths, (path) => {
+								self.append(UUI.BUTTON_H({
+									icon : IMG({
+										src : DasomEditor.R('icon/delete.png')
+									}),
+									spacing : 5,
+									title : path
+								}));
+							});
+						}
+					});
+				}
+				
+				else {
+					
+					SkyDesktop.Alert({
+						msg : 'Git 저장소가 아닙니다.'
+					});
+					
+					self.remove();
 				}
 			});
 		}
