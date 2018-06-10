e8e0c21459181129e1ae08325a4b7c586d222095
Hanul <hanul@hanul.me>
2018-06-10 21:53:14 +0900

    no message

--- DasomEditor/BROWSER/Editor/AceEditor/SolidityEditor.js
+++ DasomEditor/BROWSER/Editor/AceEditor/SolidityEditor.js
@@ -12,6 +12,20 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 		});
 	};
 	
+	cls.findContracts = (path, callback) => {
+		//REQUIRED: path
+		//REQUIRED: callback
+		
+		// 구현해야합니다.
+	};
+	
+	cls.saveContract = (data, callback) => {
+		//REQUIRED: data
+		//REQUIRED: callback
+		
+		// 구현해야합니다.
+	};
+	
 	return {
 		
 		preset : () => {
@@ -33,184 +47,185 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 			let ftpInfo = params.ftpInfo;
 			let path = params.path;
 			
-			let folderPath = path.substring(0, path.lastIndexOf('/'));
-			let fileName = path.substring(path.lastIndexOf('/') + 1);
-			
-			if (contractInfoStore === undefined) {
-				contractInfoStore = DasomEditor.STORE('contractInfoStore');
-			}
-			
-			// 코드를 저장하면 자동으로 컴파일
-			self.on('save', () => {
+			// 메타마스크 연동이 되어있고, 로컬인 경우
+			if (global.web3 !== undefined && ftpInfo === undefined) {
 				
-				let loadingBar = SkyDesktop.LoadingBar('lime');
+				let folderPath = path.substring(0, path.lastIndexOf('/'));
+				let fileName = path.substring(path.lastIndexOf('/') + 1);
 				
-				let code = self.getContent();
+				if (contractInfoStore === undefined) {
+					contractInfoStore = DasomEditor.STORE('contractInfoStore');
+				}
 				
-				let importCodes = {};
-				
-				let loadImportFiles = (code, folderPath, importBasePath, callback) => {
+				// 코드를 저장하면 자동으로 컴파일
+				self.on('save', () => {
 					
-					let importFileInfos = [];
+					let loadingBar = SkyDesktop.LoadingBar('lime');
 					
-					// 코드를 분석하여 import 구문을 찾습니다.
-					EACH(code.split('\n'), (line) => {
-						line = line.trim();
-						if (line.substring(0, 7) === 'import ') {
-							let start, end;
-							for (let i = 7; i < line.length; i += 1) {
-								if (line[i] === '"') {
-									if (start === undefined) {
-										start = i + 3;
-									} else {
-										end = i;
-										break;
+					let code = self.getContent();
+					
+					let importCodes = {};
+					
+					let loadImportFiles = (code, folderPath, importBasePath, callback) => {
+						
+						let importFileInfos = [];
+						
+						// 코드를 분석하여 import 구문을 찾습니다.
+						EACH(code.split('\n'), (line) => {
+							line = line.trim();
+							if (line.substring(0, 7) === 'import ') {
+								let start, end;
+								for (let i = 7; i < line.length; i += 1) {
+									if (line[i] === '"') {
+										if (start === undefined) {
+											start = i + 3;
+										} else {
+											end = i;
+											break;
+										}
 									}
 								}
+								
+								if (start !== undefined && end !== undefined) {
+									
+									let importPath = line.substring(start, end);
+									
+									importFileInfos.push({
+										path : folderPath + '/' + importPath,
+										importPath : (importBasePath === '' ? '' : importBasePath + '/') + importPath
+									});
+								}
+							}
+						});
+						
+						NEXT(importFileInfos, [
+						(importFileInfo, next) => {
+							
+							let path = importFileInfo.path;
+							let importPath = importFileInfo.importPath;
+							
+							if (importCodes[importPath] !== undefined) {
+								next();
 							}
 							
-							if (start !== undefined && end !== undefined) {
+							else {
 								
-								let importPath = line.substring(start, end);
-								
-								importFileInfos.push({
-									path : folderPath + '/' + importPath,
-									importPath : (importBasePath === '' ? '' : importBasePath + '/') + importPath
+								DasomEditor.IDE.load({
+									ftpInfo : ftpInfo,
+									path : path
+								}, (code) => {
+									importCodes[importPath] = code;
+									
+									// 이 코드의 import 파일들도 불러옵니다.
+									loadImportFiles(code, path.substring(0, path.lastIndexOf('/')), importPath.substring(0, importPath.lastIndexOf('/')), next);
 								});
 							}
-						}
-					});
+						},
+						
+						() => {
+							return callback;
+						}]);
+					};
 					
-					NEXT(importFileInfos, [
-					(importFileInfo, next) => {
+					loadImportFiles(code, folderPath, '', () => {
 						
-						let path = importFileInfo.path;
-						let importPath = importFileInfo.importPath;
+						console.log('Solidity 코드(' + fileName + ')를 컴파일합니다.');
 						
-						if (importCodes[importPath] !== undefined) {
-							next();
-						}
+						let errorTab;
 						
-						else {
-							
-							DasomEditor.IDE.load({
-								ftpInfo : ftpInfo,
-								path : path
-							}, (code) => {
-								importCodes[importPath] = code;
+						// 저장되어 있던 계약 제거
+						contractInfoStore.remove(path);
+						
+						Solc.Compile({
+							code : code,
+							importCodes : importCodes
+						}, {
+							error : (errorMsg) => {
+								loadingBar.done();
 								
-								// 이 코드의 import 파일들도 불러옵니다.
-								loadImportFiles(code, path.substring(0, path.lastIndexOf('/')), importPath.substring(0, importPath.lastIndexOf('/')), next);
-							});
-						}
-					},
-					
-					() => {
-						return callback;
-					}]);
-				};
-				
-				loadImportFiles(code, folderPath, '', () => {
-					
-					console.log('Solidity 코드(' + fileName + ')를 컴파일합니다.');
-					
-					let errorTab;
-					
-					// 저장되어 있던 계약 제거
-					contractInfoStore.remove(path);
-					
-					Solc.Compile({
-						code : code,
-						importCodes : importCodes
-					}, {
-						error : (errorMsg) => {
-							loadingBar.done();
-							
-							SHOW_ERROR('컴파일 오류', errorMsg);
-							
-							if (errorTab === undefined) {
+								SHOW_ERROR('컴파일 오류', errorMsg);
 								
-								DasomEditor.IDE.addTab(errorTab = SkyDesktop.Tab({
-									style : {
-										position : 'relative'
-									},
-									size : 30,
-									c : [UUI.ICON_BUTTON({
+								if (errorTab === undefined) {
+									
+									DasomEditor.IDE.addTab(errorTab = SkyDesktop.Tab({
 										style : {
-											position : 'absolute',
-											right : 10,
-											top : 8,
-											color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#444' : '#ccc',
-											zIndex : 999
+											position : 'relative'
 										},
-										icon : FontAwesome.GetIcon('times'),
-										on : {
-											mouseover : (e, self) => {
-												self.addStyle({
-													color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#666' : '#999'
-												});
+										size : 30,
+										c : [UUI.ICON_BUTTON({
+											style : {
+												position : 'absolute',
+												right : 10,
+												top : 8,
+												color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#444' : '#ccc',
+												zIndex : 999
 											},
-											mouseout : (e, self) => {
-												self.addStyle({
-													color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#444' : '#ccc'
-												});
-											},
-											tap : () => {
-												
-												errorTab.remove();
-												errorTab = undefined;
-												
-												EVENT.fireAll('resize');
+											icon : FontAwesome.GetIcon('times'),
+											on : {
+												mouseover : (e, self) => {
+													self.addStyle({
+														color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#666' : '#999'
+													});
+												},
+												mouseout : (e, self) => {
+													self.addStyle({
+														color : BROWSER_CONFIG.SkyDesktop !== undefined && BROWSER_CONFIG.SkyDesktop.theme === 'dark' ? '#444' : '#ccc'
+													});
+												},
+												tap : () => {
+													
+													errorTab.remove();
+													errorTab = undefined;
+													
+													EVENT.fireAll('resize');
+												}
 											}
-										}
-									}), H2({
-										style : {
-											padding : 10
-										},
-										c : 'Solidity 컴파일'
-									}), P({
+										}), H2({
+											style : {
+												padding : 10
+											},
+											c : 'Solidity 컴파일'
+										}), P({
+											style : {
+												padding : 10,
+												paddingTop : 0,
+												color : 'red'
+											},
+											c : '컴파일 오류가 발생했습니다. 오류 메시지: ' + errorMsg
+										})]
+									}));
+								}
+								
+								else {
+									errorTab.append(P({
 										style : {
 											padding : 10,
 											paddingTop : 0,
 											color : 'red'
 										},
 										c : '컴파일 오류가 발생했습니다. 오류 메시지: ' + errorMsg
-									})]
-								}));
+									}));
+								}
+							},
+							success : (contracts) => {
+								loadingBar.done();
+								
+								// 컴파일 결과 저장
+								contractInfoStore.save({
+									name : path,
+									value : contracts
+								});
+								
+								console.log('Solidity 코드(' + fileName + ') 컴파일을 완료하였습니다.', contracts);
+								SkyDesktop.Noti('컴파일을 완료하였습니다.');
 							}
-							
-							else {
-								errorTab.append(P({
-									style : {
-										padding : 10,
-										paddingTop : 0,
-										color : 'red'
-									},
-									c : '컴파일 오류가 발생했습니다. 오류 메시지: ' + errorMsg
-								}));
-							}
-						},
-						success : (contracts) => {
-							loadingBar.done();
-							
-							// 컴파일 결과 저장
-							contractInfoStore.save({
-								name : path,
-								value : contracts
-							});
-							
-							console.log('Solidity 코드(' + fileName + ') 컴파일을 완료하였습니다.', contracts);
-							SkyDesktop.Noti('컴파일을 완료하였습니다.');
-						}
+						});
 					});
 				});
-			});
-			
-			// 메타마스크 연동이 되어있는 경우
-			if (global.web3 !== undefined) {
 				
 				let deployButton;
 				let testButton;
+				let networkButton;
 				
 				self.on('active', () => {
 					
@@ -248,8 +263,6 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 													tap : () => {
 														
 														let abi = JSON.parse(contractInfo.interface);
-														
-														//TODO: 파라미터를 입력받도록
 														
 														let errorTab;
 														let showError = (errorMsg) => {
@@ -322,31 +335,97 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 														NEXT([
 														(next) => {
 															
-															// 가스 량 계산
-															web3.eth.estimateGas({
-																data : contractInfo.bytecode
-															}, (error, gasEstimate) => {
-																if (error !== TO_DELETE) {
-																	showError(error.toString());
-																} else {
-																	next(gasEstimate);
+															let inputInfos;
+															EACH(abi, (info) => {
+																if (info.type === 'constructor') {
+																	inputInfos = info.inputs;
 																}
 															});
+															
+															if (inputInfos === undefined || inputInfos.length === 0) {
+																next([]);
+															} else {
+																
+																// 파라미터 입력
+																let form;
+																let inputs = [];
+																SkyDesktop.Confirm({
+																	okButtonTitle : '배포',
+																	msg : form = UUI.VALID_FORM()
+																}, () => {
+																	
+																	let args = [];
+																	
+																	EACH(inputs, (input, i) => {
+																		
+																		let value = input.getValue();
+																		let type = inputInfos[i].type;
+																		
+																		if (type === 'bool') {
+																			value = value === 'true';
+																		} else if (type.indexOf('int') !== -1) {
+																			value = INTEGER(value);
+																		} else if (type.indexOf('fixed') !== -1) {
+																			value = REAL(value);
+																		} else if (type.indexOf('bytes') !== -1) {
+																			value = JSON.parse(value);
+																		} 
+																		
+																		args.push(value);
+																	});
+																	
+																	next(args);
+																});
+																
+																EACH(inputInfos, (inputInfo, i) => {
+																	
+																	inputs.push(INPUT({
+																		style : {
+																			marginTop : i === 0 ? 0 : 10,
+																			width : 222,
+																			padding : 8,
+																			border : '1px solid #999',
+																			borderRadius : 4
+																		},
+																		placeholder : inputInfo.name + ' (' + inputInfo.type + ')'
+																	}).appendTo(form));
+																});
+															}
 														},
 														
 														(next) => {
-															return (gasEstimate) => {
+															return (args) => {
+																
+																// 가스 량 계산
+																web3.eth.estimateGas({
+																	data : contractInfo.bytecode
+																}, (error, gasEstimate) => {
+																	if (error !== TO_DELETE) {
+																		showError(error.toString());
+																	} else {
+																		next(args, gasEstimate);
+																	}
+																});
+															};
+														},
+														
+														(next) => {
+															return (args, gasEstimate) => {
+																
+																let contractArgs = COPY(args);
 																
 																let loadingBar = SkyDesktop.LoadingBar('lime');
 																
 																// 계약 생성
 																let Contract = web3.eth.contract(abi);
 																
-																Contract.new({
+																contractArgs.push({
 																	from : web3.eth.accounts[0],
 																	data : contractInfo.bytecode,
 																	gas : gasEstimate
-																}, (error, contract) => {
+																});
+																
+																contractArgs.push((error, contract) => {
 																	if (error !== TO_DELETE) {
 																		loadingBar.done();
 																		showError(error.toString());
@@ -354,15 +433,28 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 																		
 																		// 배포 완료
 																		if (contract.address !== undefined) {
-																			loadingBar.done();
-																			SkyDesktop.Noti('계약을 배포하였습니다.');
-																			//TODO: 배포 내역 저장 (abi도 함께 저장합니다.)
+																			
+																			// 배포 내역 저장 (abi도 함께 저장합니다.)
+																			cls.saveContract({
+																				path : path,
+																				name : name,
+																				address : contract.address,
+																				abi : abi,
+																				args : args
+																			}, () => {
+																				
+																				loadingBar.done();
+																				SkyDesktop.Noti('계약을 배포하였습니다.');
+																			});
 																		}
 																	}
 																});
+																
+																console.log(contractArgs);
+																
+																Contract.new.apply(Contract, contractArgs);
 															};
-														}
-														]);
+														}]);
 														
 														menu.remove();
 													}
@@ -382,24 +474,52 @@ DasomEditor.SolidityEditor = CLASS((cls) => {
 						title : '계약 테스트',
 						on : {
 							tap : () => {
+								cls.findContracts(path, (contracts) => {
+									console.log(contracts);
+								});
 							}
 						}
 					}));
+					
+					let networkTitle;
+					DasomEditor.IDE.addToolbarButton(networkButton = SkyDesktop.ToolbarButton({
+						style : {
+							cursor : 'default'
+						},
+						icon : IMG({
+							src : DasomEditor.R('icon/network.png')
+						}),
+						title : networkTitle = SPAN({
+							c : 'Checking Network ..........'
+						})
+					}));
+					
+					web3.version.getNetwork((err, netId) => {
+						networkTitle.empty();
+						if (netId === '1') {
+							networkTitle.append('Main Ethereum Network');
+						} else if (netId === '3') {
+							networkTitle.append('Ropsten Test Network');
+						} else if (netId === '4') {
+							networkTitle.append('Rinkeby Test Network');
+						} else if (netId === '42') {
+							networkTitle.append('Kovan Test Network');
+						} else {
+							networkTitle.append('Unknown Network');
+						}
+					});
 				});
 				
-				self.on('deactive', () => {
+				let removeToolbarButtons = () => {
 					
 					// 도구 메뉴 제거
 					DasomEditor.IDE.removeToolbarButton(deployButton);
 					DasomEditor.IDE.removeToolbarButton(testButton);
-				});
+					DasomEditor.IDE.removeToolbarButton(networkButton);
+				};
 				
-				self.on('remove', () => {
-					
-					// 도구 메뉴 제거
-					DasomEditor.IDE.removeToolbarButton(deployButton);
-					DasomEditor.IDE.removeToolbarButton(testButton);
-				});
+				self.on('deactive', removeToolbarButtons);
+				self.on('remove', removeToolbarButtons);
 			}
 		}
 	};
--- DasomEditorServer/BROWSER/MAIN.js
+++ DasomEditorServer/BROWSER/MAIN.js
@@ -6,5 +6,22 @@ DasomEditorServer.MAIN = METHOD({
 			uri : '',
 			target : DasomEditorServer.Home
 		});
+		
+		// 솔리디티 에디터에 기능 추가
+		DasomEditor.SolidityEditor.findContracts = (path, callback) => {
+			//REQUIRED: path
+			//REQUIRED: callback
+			
+			DasomEditorServer.EthereumContractModel.find({
+				path : path
+			}, callback);
+		};
+		
+		DasomEditor.SolidityEditor.saveContract = (data, callback) => {
+			//REQUIRED: data
+			//REQUIRED: callback
+			
+			DasomEditorServer.EthereumContractModel.create(data, callback);
+		};
 	}
 });
