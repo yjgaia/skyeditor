5da84f9da464952e3bfcbd96a171dee7b5f1f215
YJ <hanul@hanul.me>
2018-07-29 22:33:17 +0900

    no message

--- DasomEditorServer/NODE/EthereumContractModel.js
+++ DasomEditorServer/NODE/EthereumContractModel.js
@@ -48,14 +48,23 @@ OVERRIDE(DasomEditorServer.EthereumContractModel, (origin) => {
 				
 				// 계약의 정보들을 불러옵니다.
 				on('getContractInfos', (path, ret) => {
-					contractInfosDB.get(path, (data) => {
-						ret(PARSE_STR(data.contractInfosStr));
+					contractInfosDB.get(path, {
+						notExists : () => {
+							ret();
+						},
+						success : (data) => {
+							ret(PARSE_STR(data.contractInfosStr));
+						}
 					});
 				});
 				
 				// 계약의 정보들을 삭제합니다.
 				on('removeContractInfos', (path, ret) => {
-					contractInfosDB.remove(path);
+					contractInfosDB.remove(path, {
+						notExists : () => {
+							// ignore.
+						}
+					});
 				});
 			});
 		}
